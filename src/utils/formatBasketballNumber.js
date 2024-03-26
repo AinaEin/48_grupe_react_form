a3398939c5682f6a96e7f9d7577a3e951c08f47c
Aina <ainaeinikyte@gmail.com>
2024-03-26 08:52:07 +0200

    feat: update

--- src/utils/formatBasketballNumber.js
+++ src/utils/formatBasketballNumber.js
@@ -1,15 +1,14 @@
-export function formatBasketballNumber(number) {
-    return (number < 10 ? '0' : '') + number;
+export function formatBasketballNumber(number, minSize) {
+  let numString = number + "";
+  const paddingSize = minSize - numString.length;
+  return numString.padStart(paddingSize >= 0 ? minSize : numString.length, "0");
 }
 
+console.log(formatBasketballNumber(10, 3)); 
+console.log(formatBasketballNumber(100, 4)); 
+console.log(formatBasketballNumber(1000, 5)); 
+console.log(formatBasketballNumber(10000, 6));
 
-// function formatBasketballNumber(number, minSize) {
-//   let numString = number + "";
-//   const paddingSize = minSize - numString.length;
-//   return numString.padStart(paddingSize >= 0 ? minSize : numString.length, "0");
+// export function formatBasketballNumber(number) {
+//   return (number < 10 ? "0" : "") + number;
 // }
-
-// console.log(formatBasketballNumber(10, 3)); 
-// console.log(formatBasketballNumber(100, 4)); 
-// console.log(formatBasketballNumber(1000, 5)); 
-// console.log(formatBasketballNumber(10000, 6));
